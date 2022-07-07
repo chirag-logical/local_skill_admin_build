e25c9a0eff6472dd399075f62e0387fa9b843d23
Chirag Mehta <chirag.mehta@logicalloop.com>
2022-07-07 13:00:54 +0530

    latest build file upload 07/07/2022

---
+++ static/js/19.d8a719b2.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonplocal-skills-admin-dashboard"]=this["webpackJsonplocal-skills-admin-dashboard"]||[]).push([[19],{1102:function(s,a,l){}}]);
+//# sourceMappingURL=19.d8a719b2.chunk.js.map
